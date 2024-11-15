f665181d96580ae26b035fe2a3ab75661a4de087
John Russo <jrusso0604@gmail.com>
2024-11-15 13:41:53 -0500

    multiple pings

--- app/server.c
+++ app/server.c
@@ -59,7 +59,7 @@ int main() {
 
 	char buffer[BUFFER_SIZE];
 	while(read(client_fd, buffer, BUFFER_SIZE) != 0){
-		send(client_fd, "+PONG\r\n", sizeof("+PONG\r\n"), 0);
+		write(client_fd, "+PONG\r\n", sizeof("+PONG\r\n"));
 	}
 	
 	printf("Client connected\n");
